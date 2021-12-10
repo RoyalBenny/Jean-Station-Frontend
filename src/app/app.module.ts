9ed6b57896c3808f76546f3fe94334fb9d30fb1a
Royal Benny <Royal.Benny@ust.com>
2021-12-10 23:02:51 +0530

    signin added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { SigninComponent } from './signin/signin.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    SigninComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
