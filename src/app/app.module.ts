490b4ab1ef873e1e77925a1278e6e80d4299be5a
unknown <Vidya.Jayakumari@ust.com>
2021-12-12 13:10:10 +0530

    modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { SigninComponent } from './signin/signin.component';
 import { HeaderComponent } from './header/header.component';
-<<<<<<< HEAD
 import { ItemListComponent } from './item-list/item-list.component';
 import { OrderDeliveryComponent } from './order-delivery/order-delivery.component';
-=======
 import { FooterComponent } from './footer/footer.component';
 import { HomePageComponent } from './home-page/home-page.component';
-<<<<<<< HEAD
 import { ProductShowComponent } from './product-show/product-show.component';
-=======
 import { CartComponent } from './cart/cart.component';
->>>>>>> 0c378c9501af76ee707798862079c01f19c6a782
+
 
 @NgModule({
   declarations: [
@@ -24,18 +19,14 @@ import { CartComponent } from './cart/cart.component';
     LoginComponent,
     SigninComponent,
     HeaderComponent,
-<<<<<<< HEAD
     ItemListComponent,
-    OrderDeliveryComponent
-=======
+    OrderDeliveryComponent,
     FooterComponent,
     HomePageComponent,
-<<<<<<< HEAD
-    ProductShowComponent
-=======
+    ProductShowComponent,
     CartComponent
->>>>>>> 0c378c9501af76ee707798862079c01f19c6a782
   ],
+  
   imports: [
     BrowserModule,
     AppRoutingModule
